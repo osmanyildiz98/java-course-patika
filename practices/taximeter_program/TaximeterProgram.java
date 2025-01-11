07aec794276de983dcbab80d343275892e0b4043
Osman Yıldız <osmanyldzs@outlook.com>
2025-01-11 23:12:22 +0300

    Practice Taximeter Program is done

---
+++ practices/taximeter_program/TaximeterProgram.java
@@ -0,0 +1,19 @@
+package taximeter_program;
+import java.util.Scanner;
+
+public class TaximeterProgram {
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+        double kmRate = 2.20;
+        int minimumFare = 10;
+
+        System.out.print("Please enter the distance in kilometers: ");
+        double distance = input.nextDouble();
+        System.out.println("---------------------------------------");
+
+        double result = minimumFare + (distance * kmRate);
+
+        System.out.println("The amount you need to pay is " + (result < 20 ? 20 : result));
+
+    }
+}
